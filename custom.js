02b8ec33eae868df03b1da0306c0bde4a22b06c2
RONIT SINGH <ronitsingh7003@gmail.com>
2023-02-09 23:27:24 +0530

    bg_added

--- custom.js
+++ custom.js
@@ -2,3 +2,9 @@
 // }
 document.getElementById("my_audio").play();
 
+var preloader = document.getElementById('loader_bg');
+var wishBG = document.getElementById('wishBG');
+setTimeout(function loaderFun() {
+    preloader.style.display = 'none';
+}, 2000);
+
